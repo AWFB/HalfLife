d69ad6381cf13c89289e93f113462b5944731b98
Antony Beardsmore <sider@me.com>
2021-11-09 00:47:42 +0000

    Add project files.

---
+++ HalfLife/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HalfLife
+{
+    class Program
+    {
+        // TODO - give chemo or not logic
+        static void Main(string[] args)
+        {
+            Console.Write("What is the first concentration (ng/L)?:");
+            double initialConcentration = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("How many hours post surgery?:");
+            double time = Convert.ToDouble(Console.ReadLine()); 
+
+            Console.Write("What is the concentration now (ng/L)?:");
+            double concentrationNow = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("How many hours post surgery?:");
+            double timeTwo = Convert.ToDouble(Console.ReadLine());
+
+            var calculate = new HalfLifeLibrary.HalfLifeCalculation();
+            var result = calculate.Calc(time, timeTwo, concentrationNow, initialConcentration);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HalfLifeLibrary/HalfLifeCalculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HalfLifeLibrary
+{
+    public class HalfLifeCalculation
+    {
+        public double Calc(double time, double timeTwo, double concentrationNow, double initialConcentration)
+        {
+            double LogOfInitialConc = Math.Round(Math.Log(initialConcentration), 2);
+            double LogOfLatestResult = Math.Round(Math.Log(concentrationNow), 2);
+            double kd = (LogOfInitialConc - LogOfLatestResult) / (timeTwo - time);
+            double halflife = Math.Round(0.693 / kd);  // This relationship can be obtained by substituting   t = t1/2 and loge Ct =  logeC0/2 into the
+                                                       // first order rate equation.  NB loge2 = 0.693)
+
+            //string output = ($"The half life for this tumour marker is { halflife } hours");
+            return halflife;
+
+        }        
+    }
+}
